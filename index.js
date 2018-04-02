dcfc5b0553f094fba90dc4cebfec75d7f41c412a
Josh Zich <zichjd@gmail.com>
2018-04-02 13:17:09 -0400

    corrected use from get

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get(express.static('public'))
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server listening on port 3000'))
