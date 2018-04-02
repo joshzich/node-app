4144d8af63372cfb003c2c35baad94b3b9166208
Josh Zich <zichjd@gmail.com>
2018-04-02 13:10:51 -0400

    added html page

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-	res.send('hey')
-})
-
+app.get(express.static('public'))
 app.listen(3000, () => console.log('Server listening on port 3000'))
